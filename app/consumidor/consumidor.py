94e2b0769572c475ed06cfe9529f778756d16249
Edilson <nantes.junior@gmail.com>
2023-12-05 15:47:20 -0300

    Adição de mais rotas

--- app/consumidor/consumidor.py
+++ app/consumidor/consumidor.py
@@ -1,5 +1,14 @@
 import requests
+import pandas as pd
 
-response = requests.get("http://main:5000/dados")
-resultado = response.json()
-print(f"Resultado obtido do servidor: {resultado}")
+response = requests.get("http://main:5000/media")
+mean = response.json()
+print(f"Média de idades: {mean}")
+
+response = requests.get("http://main:5000/media/house")
+mean_house = response.json()
+print(f"Análise da média de idade por tipos de moradia: {mean_house}")
+
+response = requests.get("http://main:5000/credit/age")
+creditByAge = response.json()
+print(f"Análise do registro de inadimplência por idade: {creditByAge}")
--- app/main/main.py
+++ app/main/main.py
@@ -6,12 +6,20 @@ app = Flask(__name__)
 dados = pd.read_csv("/app/Credit.csv")
 dados.shape
 
-@app.route('/dados', methods=['GET'])
-def obter_dados():
-    
-
+@app.route('/media', methods=['GET'])
+def getMean():
     resultado = dados['age'].mean()
     return jsonify({'resultado': resultado})
+
+@app.route('/media/house', methods=['GET'])
+def getMeanByHouse():
+    resultado = dados.groupby('housing',as_index=False)['age'].mean()
+    return jsonify({'resultado': resultado.to_json(orient='records')})
+
+@app.route('/credit/age', methods=['GET'])
+def getCreditByAge():
+    resultado = dados.groupby('credit_history', as_index=False)['age'].agg(['min','max'])
+    return jsonify({'resultado': resultado.to_json(orient='records')})
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000)
